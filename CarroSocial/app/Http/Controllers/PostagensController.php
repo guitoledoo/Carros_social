cb54bae3de7a9cdea72e3a0f988975ce79ed0174
guitoledoo <guilhermet.vidal@gmail.com>
2024-10-09 23:21:39 -0300

    MEU PROJETO

---
+++ CarroSocial/app/Http/Controllers/PostagensController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostagensController extends Controller
+{
+    //
+}
---
+++ CarroSocial/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carros Social Home</title>
+</head>
+<body>
+    <h1>Bem-vindo ao Carros Social!</h1>
+    <p>Um espaço para amantes de carros compartilharem suas paixões.</p>
+</body>
+</html>
